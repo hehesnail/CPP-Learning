17d0ec0018c8fed0cfa47f6b8025073f181f4bc7
hehesnail <shashen008he@gmail.com>
2018-02-10 17:57:08 +0800

    Update smartPointer.cc

--- smartPointer.cc
+++ smartPointer.cc
@@ -7,13 +7,12 @@ using namespace std;
 
 int main() {
 
-	shared_ptr<int> p3 = make_shared<int> (42);
-	shared_ptr<string> p4 = make_shared<string> (10, '9');
+    shared_ptr<int> p3 = make_shared<int> (42);
+    shared_ptr<string> p4 = make_shared<string> (10, '9');
     auto p6 = make_shared<vector<string>> ();
     auto p = make_shared<int> (42);
     auto q(p);
     auto r = make_shared<int> (42);
-    r =
-
-	return 0;
+    
+    return 0;
 }
